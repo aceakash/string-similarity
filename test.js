f09e9e927003fb446b2bf510772a4ff3f92838e9
ascriver <ascriver@live.com>
2019-01-06 18:26:40 -0700

    Changed legacy function substr() to substring()

--- test.js
+++
@@ -1,5 +0,0 @@
-const comp = require('./compare-strings.js');
-
-let robles = comp.compareTwoStrings('Olive-green table for sale, in extremely good condition.','For sale: green Subaru Impreza, 210,000 miles')
-
-console.log(robles);
